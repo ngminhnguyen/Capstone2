08d06330a39d6fc7904ce24ede12118e710d6c1d
ngminhnguyen <minhnguyen2811200909@gmail.com>
2026-04-20 14:48:51 +0700

    Update Step1.tsx

--- src/components/register/Step1.tsx
+++ src/components/register/Step1.tsx
@@ -61,7 +61,7 @@ export default function Step1({ nextStep, prevStep }: Props) {
     };
 
     return (
-        <div className="min-h-screen sm:w-[80%] mx-auto bg-[#fdece4] text-[#5a0000] flex flex-col items-center py-5">
+        <div className="sm:w-[80%] mx-auto bg-[#fdece4] text-[#5a0000] flex flex-col items-center py-5">
             {/* Progress */}
             <div className="relative w-[60%] h-20 flex items-center justify-between mb-10 pl-20 pr-20">
                 <div className="absolute top-1/2 left-0 w-full h-2 bg-cyan-700 -translate-y-1/2 rounded-full"></div>
@@ -85,12 +85,7 @@ export default function Step1({ nextStep, prevStep }: Props) {
                     <img
                         src="/images/circleCreate2.png"
                         alt="Step 2"
-                        className="absolute inset-0 w-full h-full transition-opacity duration-300 group-hover:opacity-0"
-                    />
-                    <img
-                        src="/images/circleCreate1.png"
-                        alt="Step 2 Hover"
-                        className="absolute inset-0 w-full h-full opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+                        className="absolute inset-0 w-full h-full"
                     />
                     <span className="absolute -rotate-20 font-bold z-10">
                         Step 2
@@ -255,18 +250,21 @@ export default function Step1({ nextStep, prevStep }: Props) {
                     </button>
                 </div>
             </div>
-            <button
-                onClick={prevStep}
-                className="px-8 py-3 rounded-xl bg-gray-300 font-bold"
-            >
-                Back
-            </button>
-            <button
-                onClick={nextStep}
-                className="bg-yellow-400 px-9 py-2 rounded-xl font-bold hover:bg-yellow-300 transition-colors duration-200"
-            >
-                Next
-            </button>
+            {/* Button Step*/}
+            <div className="w-[60%] flex justify-between mt-8 pr-10 pl-10">
+                <button
+                    onClick={prevStep}
+                    className="bg-gray-200 px-9 py-2 rounded-xl font-bold hover:bg-gray-100 transition-colors duration-200"
+                >
+                    Back
+                </button>
+                <button
+                    onClick={nextStep}
+                    className="bg-yellow-400 px-9 py-2 rounded-xl font-bold hover:bg-yellow-300 transition-colors duration-200"
+                >
+                    Next
+                </button>
+            </div>
         </div>
     );
 }
