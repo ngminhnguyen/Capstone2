940fe2fcc7d426ddeea650c039a9f11a4c15f2ef
ngminhnguyen <minhnguyen2811200909@gmail.com>
2026-04-21 08:45:32 +0700

    Update page.tsx

--- src/app/check/page.tsx
+++ src/app/check/page.tsx
@@ -1,30 +1,23 @@
 export default function Check() {
     return (
-        <div className="w-full bg-[#f5ebe6] px-6 py-4 flex flex-col md:flex-row justify-between items-center gap-4">
-            {/* LEFT - FILTER */}
-            <div className="flex items-center gap-3 flex-wrap">
+        <div className="mx-auto max-w-6xl px-6 py-4 flex flex-col md:flex-row justify-between items-center gap-4">
+            {/* Filter */}
+            <div className="flex flex-col items-center gap-3">
                 <span className="font-medium text-[#4E0706]">Filter by:</span>
 
-                <select className="px-4 py-2 rounded-xl border border-[#4E0706] bg-[#fff3ed] text-[#4E0706]">
-                    <option>Age (s)</option>
-                </select>
+                <div className="flex items-center gap-3 flex-wrap">
+                    <select className="px-4 py-2 rounded-xl border border-[#4E0706] bg-[#fff3ed] text-[#4E0706]">
+                        <option>Age (s)</option>
+                    </select>
 
-                <select className="px-4 py-2 rounded-xl border border-[#4E0706] bg-[#fff3ed] text-[#4E0706]">
-                    <option>Dietary needs (s)</option>
-                </select>
+                    <select className="px-4 py-2 rounded-xl border border-[#4E0706] bg-[#fff3ed] text-[#4E0706]">
+                        <option>Dietary needs (s)</option>
+                    </select>
 
-                <select className="px-4 py-2 rounded-xl border border-[#4E0706] bg-[#fff3ed] text-[#4E0706]">
-                    <option>Mealtime (s)</option>
-                </select>
-            </div>
-
-            {/* RIGHT - ORDER */}
-            <div className="flex items-center gap-3">
-                <span className="font-medium text-[#4E0706]">Order by:</span>
-
-                <select className="px-4 py-2 rounded-xl border border-[#4E0706] bg-[#fff3ed] text-[#4E0706]">
-                    <option>What's new</option>
-                </select>
+                    <select className="px-4 py-2 rounded-xl border border-[#4E0706] bg-[#fff3ed] text-[#4E0706]">
+                        <option>Mealtime (s)</option>
+                    </select>
+                </div>
             </div>
         </div>
     );
