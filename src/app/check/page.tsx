4df047178ed125b48b3d16d223f0c9c95ac7bd6c
ngminhnguyen <minhnguyen2811200909@gmail.com>
2026-04-23 22:20:36 +0700

    Update page.tsx

--- src/app/check/page.tsx
+++ src/app/check/page.tsx
@@ -1,90 +1,213 @@
 "use client";
-import React from "react";
+import { color } from "framer-motion";
+import React, { useState } from "react";
 
 const recipes = [
     {
-        title: "Golden pumpkin spiced porridge",
-        img: "/images/popularRecipe1.webp",
-        age: "7+ months",
+        id: "1",
+        title: "bright starts brekkie bowl",
+        desc: "Perfect for hot summer days...",
+        img: "/images/recipe1.webp",
+        mealTime: "Luch",
         color: "bg-purple-500",
     },
     {
-        title: "Broccoli puree recipe",
-        img: "/images/popularRecipe2.webp",
-        age: "4-6 months",
+        id: "2",
+        title: "three ways with yummy yogurt pots",
+        desc: "Yogurt is one of our go-to...",
+        img: "/images/recipe2.webp",
+        mealTime: "Snack",
+        color: "bg-yellow-500",
+    },
+    {
+        id: "3",
+        title: "rise + shine scrambly eggs",
+        desc: "These speedy scrambly eggs...",
+        img: "/images/recipe3.webp",
+        mealTime: "Breakfast",
         color: "bg-pink-500",
     },
     {
-        title: "Easy Cheesy Eggy Bread",
-        img: "/images/popularRecipe3.webp",
-        age: "7+ months",
-        color: "bg-purple-500",
+        id: "4",
+        title: "broc n roll cheesy chive pasta",
+        desc: "Ready in just 15 minutes...",
+        img: "/images/recipe4.webp",
+        mealTime: "Dinner",
+        color: "bg-pink-500",
     },
     {
-        title: "Dairy Free Sweetcorn and Chive Pancake Fingers",
-        img: "/images/popularRecipe4.webp",
-        age: "7+ months",
-        color: "bg-purple-500",
+        id: "5",
+        title: "my first chicken curry",
+        desc: "This recipe is a great way...",
+        img: "/images/recipe5.webp",
+        mealTime: "Snack",
+        color: "bg-pink-500",
     },
     {
-        title: "My first chicken curry",
-        img: "/images/popularRecipe5.webp",
-        age: "7+ months",
-        color: "bg-purple-500",
+        id: "6",
+        title: "my first chicken curry",
+        desc: "This recipe is a great way...",
+        img: "/images/recipe6.webp",
+        mealTime: "Luch",
+        color: "bg-orange-500",
     },
 ];
 
-export default function PopularRecipes() {
+export default function RecipeWithPagination() {
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const itemsPerPage = 9;
+
+    // 🔥 chia data theo page
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const currentData = recipes.slice(startIndex, startIndex + itemsPerPage);
+
+    const totalPages = Math.ceil(recipes.length / itemsPerPage);
+
     return (
-        <section className="w-full bg-[#EFD36A] py-16">
-            {/* TITLE */}
-            <h2 className="text-center text-4xl md:text-5xl font-serif text-[#5A0A0A] mb-12">
-                most popular recipes
-            </h2>
+        <section className="w-full bg-[#F6EDE7] py-16 px-6">
+            {/* GRID */}
+            <div className="relative grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
+                {currentData.map((item) => (
+                    <div
+                        key={item.id}
+                        className={`relative bg-linear-to-b ${item.color}
+                        text-white rounded-2xl p-4
+                        h-[500px]
+                        transition-all duration-300
+                        hover:-translate-y-2
+                        hover:rotate-2
+                        hover:shadow-[0_25px_50px_rgba(0,0,0,0.3)]
+                        "`}
+                    >
+                        {/* Meal Time */}
+                        <div className="absolute -top-3 right-4 bg-pink-200 text-[#5A0A0A] text-sm px-6 py-1 rotate-5 rounded-sm border-2 border-amber-950 ">
+                            {item.mealTime}
+                        </div>
 
-            {/* CARDS */}
-            <div className="flex justify-center gap-6 px-6 overflow-x-auto">
-                {recipes.map((item, index) => (
-                    <div key={index} className="flex flex-col items-center">
-                        {/* CARD */}
-                        <div className="relative ">
-                            {/* IMAGE */}
-                            <div className="border-3 border-amber-950 rounded-2xl">
-                                <img
-                                    src={item.img}
-                                    alt={item.title}
-                                    className="w-full object-cover rounded-2xl"
-                                    width={400}
-                                    height={300}
-                                />
-                            </div>
-
-                            {/* BADGE */}
-                            <div
-                                className={`absolute -top-2 left-1/2 -translate-x-1/2 ${item.color} text-white text-sm px-4 py-2 rounded-full border-2 border-[#5A0A0A] rotate-[8deg]`}
-                            >
-                                {item.age}
-                            </div>
+                        {/* IMG */}
+                        <div className="rounded-xl overflow-hidden mb-4">
+                            <img
+                                src={item.img}
+                                className="w-full h-full object-cover"
+                            />
                         </div>
 
                         {/* TITLE */}
-                        <p className="mt-4 text-center text-[#5A0A0A] font-medium max-w-50">
-                            {item.title}
+                        <h3 className="font-bold text-sm mb-2">{item.title}</h3>
+
+                        {/* DESC */}
+                        <p className="text-xs text-gray-200 mb-4">
+                            {item.desc}
                         </p>
+
+                        <div className="absolute bottom-4 left-4 right-3 mt-4">
+                            {/* DOTTED LINE */}
+                            <div
+                                className="w-full h-2 mb-4 
+                                        bg-[radial-gradient(circle,#ffffff_3px,transparent_3px)] 
+                                        bg-size-[14px_8px] bg-repeat-x"
+                            ></div>
+                            {/* INFO ROW */}
+                            <div className="flex items-center justify-start gap-3">
+                                {/* Prep */}
+                                <div className="flex flex-col items-center gap-1">
+                                    <span className="text-xs opacity-80">
+                                        Prep
+                                    </span>
+                                    <div className="relative">
+                                        <img
+                                            src="/images/recipeCircle2.png"
+                                            alt="Recipe Circle 2"
+                                            className="w-10 h-10"
+                                        />
+                                        <span className="absolute top-2 left-3.5 text-[#5A0A0A] flex flex-col items-center leading-none">
+                                            <span className="text-[16px] font-bold">
+                                                6
+                                            </span>
+                                            <span className="text-[8px]">
+                                                min
+                                            </span>
+                                        </span>
+                                    </div>
+                                </div>
+
+                                {/* Cook */}
+                                <div className="flex flex-col items-center gap-1">
+                                    <span className="text-xs opacity-80">
+                                        Cook
+                                    </span>
+                                    <div className="relative">
+                                        <img
+                                            src="/images/recipeCircle2.png"
+                                            alt="Recipe Circle 2"
+                                            className="w-10 h-10"
+                                        />
+                                        <span className="absolute top-2 left-3.5 text-[#5A0A0A] flex flex-col items-center leading-none">
+                                            <span className="text-[16px] font-bold">
+                                                6
+                                            </span>
+                                            <span className="text-[8px]">
+                                                min
+                                            </span>
+                                        </span>
+                                    </div>
+                                </div>
+
+                                {/* AGE */}
+                                <div className="flex flex-col items-center gap-1">
+                                    <span className="text-xs opacity-80 invisible">
+                                        Age
+                                    </span>
+                                    <div
+                                        className={`w-12 h-12 text-white rounded-full flex items-center justify-center text-[11px] font-semibold border-2 border-[#5A0A0A] ${item.color}`}
+                                    >
+                                        <div className="text-center leading-none">
+                                            <div>12+</div>
+                                            <div>months</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
 
-            {/* DOTS */}
+            {/* PAGINATION */}
             <div className="flex justify-center gap-4 mt-10">
-                {[0, 1, 2, 3, 4].map((dot) => (
-                    <div
-                        key={dot}
-                        className={`w-5 h-5 rounded-full border-2 border-white ${
-                            dot === 2 ? "bg-orange-400" : "bg-[#E6C06A]"
-                        }`}
-                    />
-                ))}
+                {Array.from({ length: totalPages }, (_, i) => i + 1).map(
+                    (page) => (
+                        <button
+                            key={page}
+                            onClick={() => setCurrentPage(page)}
+                            className={`
+                                w-10 h-10 rounded-full border-2 flex items-center justify-center
+                                transition-all duration-200
+
+                                ${
+                                    currentPage === page
+                                        ? "bg-orange-400 border-[#5A0A0A]"
+                                        : "border-[#5A0A0A] text-[#5A0A0A] hover:bg-[#f3d9c9]"
+                                }
+                            `}
+                        >
+                            {page}
+                        </button>
+                    ),
+                )}
+
+                {/* NEXT */}
+                <button
+                    onClick={() =>
+                        setCurrentPage((prev) =>
+                            prev < totalPages ? prev + 1 : prev,
+                        )
+                    }
+                    className="ml-2 text-[#5A0A0A] hover:underline"
+                >
+                    next &gt;
+                </button>
             </div>
         </section>
     );
